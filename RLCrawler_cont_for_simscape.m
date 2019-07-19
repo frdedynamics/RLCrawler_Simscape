e699f121c7c379b40f277dcf357e44228d6fbcd5
dschaele <48531705+dschaele@users.noreply.github.com>
2019-07-19 09:51:45 +0200

    some changes, not converging

--- RLCrawler_cont_for_simscape.m
+++ RLCrawler_cont_for_simscape.m
@@ -1,7 +1,9 @@
+clear all
+
 
 obsInfo =  rlNumericSpec([2 1],...
-    'LowerLimit',[-90 -30]',...
-    'UpperLimit',[10 90]');
+    'LowerLimit',[-95 -95]',...
+    'UpperLimit',[15 95]');
 obsInfo.Name = 'observations';
 obsInfo.Description = 'Servo 1, Servo 2';
 numObservations = obsInfo.Dimension(1);
@@ -11,25 +13,36 @@ actInfo = rlFiniteSetSpec([1 2 3 4]); %N S W E [1 2 3 4]'
 actInfo.Name = 'Position Servo 1, Position Servo 2';
 numActions = numel(actInfo);  
 
+Ts = 1; %Sampletime
+Tf = 100; %Simulationtime
 env = rlSimulinkEnv('Crawler_Simscape','Crawler_Simscape/RL Agent',...
     obsInfo,actInfo);
-
+%w0=[0, 0]';
 w0=[0, 0, 0, 0]';
+%w0=zeros(9,1);
+%w0a=zeros(2,4);
 w0a=[0, 0, 0, 0; 0, 0, 0, 0; 0, 0, 0, 0; 0, 0, 0, 0];
-critic = rlRepresentation(@linFeatures, w0, obsInfo);
-actor = rlRepresentation(@linFeatures, w0a, obsInfo, actInfo);
+%w0a=zeros(9,4);
+repOpts = rlRepresentationOptions('LearnRate',1); %5e-5
+critic = rlRepresentation(@linFeatures, w0, obsInfo, repOpts);
+actor = rlRepresentation(@linFeatures, w0a, obsInfo, actInfo,repOpts);
 
-
-agentOpts = rlACAgentOptions;
-agentOpts.NumStepsToLookAhead = 8;
+agentOpts = rlACAgentOptions(...
+    'SampleTime',Ts,...
+    'EntropyLossWeight', 0.2,...
+    'NumStepsToLookAhead', Tf);
 
 agent = rlACAgent(actor, critic, agentOpts)
 
+maxsteps = ceil(Tf/Ts);
 trainOpts = rlTrainingOptions;
-trainOpts.MaxStepsPerEpisode = 50;
+trainOpts.MaxStepsPerEpisode = maxsteps;
 trainOpts.MaxEpisodes= 500;
 trainOpts.StopTrainingCriteria = "AverageReward";
-trainOpts.StopTrainingValue = 2;
+trainOpts.StopTrainingValue = 200;
 trainOpts.ScoreAveragingWindowLength = 30;
+trainOpts.StopOnError = "off";
+%trainOpts.Plots = "none";
+%trainOpts.Verbose = 1;
 
 trainingStats = train(agent,env,trainOpts);
