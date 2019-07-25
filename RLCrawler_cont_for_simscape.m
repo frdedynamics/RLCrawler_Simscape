338b7285dde50684a85789f001c5495985ceb85e
dschaele <48531705+dschaele@users.noreply.github.com>
2019-07-25 16:04:30 +0200

    modifications, cont not converging

--- RLCrawler_cont_for_simscape.m
+++ RLCrawler_cont_for_simscape.m
@@ -2,8 +2,8 @@ clear all
 
 
 obsInfo =  rlNumericSpec([2 1],...
-    'LowerLimit',[-95 -95]',...
-    'UpperLimit',[15 95]');
+    'LowerLimit',[-90 -30]',...
+    'UpperLimit',[10 90]');
 obsInfo.Name = 'observations';
 obsInfo.Description = 'Servo 1, Servo 2';
 numObservations = obsInfo.Dimension(1);
@@ -17,22 +17,29 @@ Ts = 1; %Sampletime
 Tf = 100; %Simulationtime
 env = rlSimulinkEnv('Crawler_Simscape','Crawler_Simscape/RL Agent',...
     obsInfo,actInfo);
+
 %w0=[0, 0]';
 w0=[0, 0, 0, 0]';
 %w0=zeros(9,1);
+%w0=zeros(14,1);
+%w0=zeros(25,1);
 %w0a=zeros(2,4);
 w0a=[0, 0, 0, 0; 0, 0, 0, 0; 0, 0, 0, 0; 0, 0, 0, 0];
 %w0a=zeros(9,4);
-repOpts = rlRepresentationOptions('LearnRate',1); %5e-5
+%w0a=zeros(14,4);
+%w0a=zeros(25,4);
+
+repOpts = rlRepresentationOptions('LearnRate',0.1); %5e-5
 critic = rlRepresentation(@linFeatures, w0, obsInfo, repOpts);
 actor = rlRepresentation(@linFeatures, w0a, obsInfo, actInfo,repOpts);
 
 agentOpts = rlACAgentOptions(...
     'SampleTime',Ts,...
-    'EntropyLossWeight', 0.2,...
-    'NumStepsToLookAhead', Tf);
+    'EntropyLossWeight', 1);%,...
+    %'NumStepsToLookAhead', 1);
 
 agent = rlACAgent(actor, critic, agentOpts)
+validateEnvironment(env)
 
 maxsteps = ceil(Tf/Ts);
 trainOpts = rlTrainingOptions;
@@ -41,8 +48,11 @@ trainOpts.MaxEpisodes= 500;
 trainOpts.StopTrainingCriteria = "AverageReward";
 trainOpts.StopTrainingValue = 200;
 trainOpts.ScoreAveragingWindowLength = 30;
-trainOpts.StopOnError = "off";
+%trainOpts.SaveAgentCriteria = "EpisodeCount";
+%trainOpts.SaveAgentValue = 3;
+%trainOpts.StopOnError = "off";
 %trainOpts.Plots = "none";
 %trainOpts.Verbose = 1;
 
 trainingStats = train(agent,env,trainOpts);
+%generatePolicyFunction(agent)
--- linFeatures.m
+++ linFeatures.m
@@ -1,6 +1,48 @@
 function X = linFeatures(S)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
+%X = [S(1); S(2)];
 X = [1; S(1); S(2); S(1)*S(2)];
+
+%X = [1; S(1); S(2); S(1)*S(2); S(1)^2; S(2)^2; S(1)*S(2)^2; S(1)^2 *S(2); S(1)^2 *S(2)^2];
+%X = [1; S(1); S(2); S(1)*S(2); S(1)^2; S(2)^2; S(1)*S(2)^2; S(1)^2 *S(2); S(1)^2 *S(2)^2; S(1)^3; S(2)^3; S(1)*S(2)^3; S(1)^3 *S(2); S(1)^3 *S(2)^3];
+
+ %S=[10 90];
+%% Emulating a tabular representation 
+% ser1 = [10 -15 -40 -65 -90];
+% ser2 = [90 60 30 0 -30];
+% 
+% if S(1) == 10
+%     idxS1 = 1;
+% elseif S(1) == -15
+%     idxS1 = 2; 
+% elseif S(1) == -40
+%     idxS1 = 3; 
+% elseif S(1) == -65
+%     idxS1 = 4; 
+% else 
+%     idxS1 = 5; 
+% end
+% 
+% if S(2) == 90
+%     idxS2 = 1;
+% elseif S(2) == 60
+%     idxS2 = 2; 
+% elseif S(2) == 30
+%     idxS2 = 3; 
+% elseif S(2) == 0
+%     idxS2 = 4; 
+% else 
+%     idxS2 = 5; 
+% end
+% 
+% idxS1 = find(ser1 == S(1));
+% idxS2 = find(ser2 == S(2));
+% idxX = sub2ind([5,5], idxS1,idxS2);
+% Xtemp = zeros(25,1) + 0* S(1)+0*S(2);
+% Xtemp2 = zeros(25,1);
+% Xtemp2(idxX) = 1;
+% Xtemp(idxX) = 1;
+% X = Xtemp + Xtemp2
 end
 
